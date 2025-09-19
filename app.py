7df7b350270cd532a2270cd6c2b63a532767ef56
McJosh <truongduydat2402@gmail.com>
2025-09-19 11:06:26 +0700

    add basic form and confirmation

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "<h1> My Finance Tracker</h1><p>It works!</p>"
+
+@app.route('/add-expense', methods=['GET', 'POST'])
+def addExpense():
+    if request.method == 'POST':
+        item = request.form['item']
+        amount = request.form['amount']
+        return f"Added {item} for {amount} VND"
+
+    return render_template('add-expense.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
