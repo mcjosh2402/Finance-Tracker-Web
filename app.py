52591ef68e44c057e5b2641879a84eea3db4daf1
McJosh <truongduydat2402@gmail.com>
2025-09-19 15:34:36 +0700

    add income form

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import json
 from datetime import datetime
 import os
 
-def saveExpense(item, amount):
+def saveTransaction(item, amount, type):
     # setup file paths and ensure /data/transactions.json exist 
     base_dir = os.path.dirname(os.path.abspath(__file__)) # get the path of this file
     fileName = os.path.join(base_dir, "data", "transactions.json") # create the full path to .../data/transactions.json
@@ -12,39 +12,39 @@ def saveExpense(item, amount):
     expense = { # dictionary (similar to c++ map)
         "item": item,
         "amount": float(amount),
-        "date": datetime.today().strftime("%Y-%m-%d"),
-        "type": "Expense"
+        "date": datetime.today().strftime("%Y-%m-%d"), # convert datetime obj to string
+        "type": type
     }
 
     try:
-        # Read existing transactions from JSON file
         with open(fileName, "r") as file:
             transactions = json.load(file)
-    except FileNotFoundError:
-        # Create empty list if file doesn't exist yet
+    except (FileNotFoundError, json.JSONDecodeError):
+        # handle missing file and empty .json
         transactions = []
 
-    # Append new expense and save back to JSON file
     transactions.append(expense)
     with open(fileName, "w") as file:
         json.dump(transactions, file, indent=2) 
 
 app = Flask(__name__) # create a webpage named app
 
-@app.route('/')
+@app.route('/') # main landing page
 def home():
     return "<h1> My Finance Tracker</h1><p>It works!</p>"
 
-@app.route('/add-expense', methods=['GET', 'POST'])
-def addExpense():
+@app.route('/add-transaction', methods=['GET', 'POST']) # add transaction page
+def addTransaction():
     if request.method == 'POST':
+        # get the submitted form info that fit the dictionary's key
         item = request.form['item']
         amount = request.form['amount']
+        type = request.form['type']
 
-        saveExpense(item, amount)
-        return f"Added {item} for {amount} VND"
+        saveTransaction(item, amount, type)
+        return f"Added {item} for {amount} VND as {type}" # confirmation
 
-    return render_template('add-expense.html')
+    return render_template('add-transaction.html') # render add-expense.html 
 
 if __name__ == "__main__":
     app.run(debug=True)
